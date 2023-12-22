db45b657b66e748bc37d9eb91c1a5b9ec738f55a
shoji <himanushi@users.noreply.github.com>
2023-12-22 16:39:19 +0900

    fix

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let count = 0;
     const addButton = document.getElementById('countButton');
     const minusButton = document.getElementById('minusButton');
+    const resetButton = document.getElementById('resetButton');
     const display = document.getElementById('countDisplay');
 
     addButton.onclick = function() {
-        count++;
-        display.innerText = count;
+        if (count < 10) { // Set maximum count to 10
+            count++;
+            display.innerText = count;
+        }
     };
 
     minusButton.onclick = function() {
@@ -14,5 +17,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
             count--;
             display.innerText = count;
         }
+    };
+    
+    resetButton.onclick = function() {
+        count = 0; // Set the count back to 0
+        display.innerText = count;
     };
 });
